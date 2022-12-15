c97f0ea0804f15f5bf026b5c353dbfd8e3100351
Frank Stein <dr.frank.stain@gmail.com>
2022-12-15 22:51:29 +0300

    Dependencies moved to main header.

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <memory>
-#include <type_traits>
-#include <utility>
+#include <main.h>
 
 
 namespace Demo
---
+++ source/main.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <utility>

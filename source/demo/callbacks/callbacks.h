2d6a7e259f381ea6681557adbf1d2caabc36011d
Frank Stein <dr.frank.stain@gmail.com>
2022-12-15 23:12:15 +0300

    Main header for callbacks.

---
+++ source/demo/callbacks/callbacks.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+// External dependencies.
+
+
+// Namespace definition.
+namespace Deom
+{
+// Callbacks subsystem.
+namespace Callbacks
+{
+// Internal stuff for callbacks.
+namespace Internal
+{
+
+}
+}
+}
+
+
+// Most fundamental internal stuff.
+
+
+// Usable internal stuff.
+
+
+// Public interface.
+
+
+// Deferred inline definitions.

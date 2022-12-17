2e71365a5ce8390a5f3fbc7d5cc77062bd16b3df
Frank Stein <dr.frank.stain@gmail.com>
2022-12-17 11:30:11 +0300

    Docs for signature selector.

--- source/demo/callbacks/internal/SignatureSelector.h
+++ source/demo/callbacks/internal/SignatureSelector.h
@@ -7,39 +7,65 @@ inline namespace Callbacks
 {
 namespace Internal
 {
+	/**
+		@brief	Signature selector for various function types.
+
+		False-deduction branch. Shall never be deducted and used.
+	*/
 	template< typename >
 	struct SignatureSelector final
 	{
+		// Dummy signature.
 		using Signature = void ();
 	};
 
+	/**
+		@brief	Signature selector for pointers to global function.
+	*/
 	template< typename TResult, typename... TArguments >
 	struct SignatureSelector<TResult (*)( TArguments... )> final
 	{
+		// Signature for given function type.
 		using Signature = TResult ( TArguments... );
 	};
 
+	/**
+		@brief	Signature selector for pointers to regular member function.
+	*/
 	template< typename THost, typename TResult, typename... TArguments >
 	struct SignatureSelector<TResult (THost::*)( TArguments... )> final
 	{
+		// Signature for given member-function type.
 		using Signature = TResult ( TArguments... );
 	};
 
+	/**
+		@brief	Signature selector for pointers to member function with constant context.
+	*/
 	template< typename THost, typename TResult, typename... TArguments >
 	struct SignatureSelector<TResult (THost::*)( TArguments... ) const> final
 	{
+		// Signature for given member-function type.
 		using Signature = TResult ( TArguments... );
 	};
 
+	/**
+		@brief	Signature selector for pointers to member function with volatile context.
+	*/
 	template< typename THost, typename TResult, typename... TArguments >
 	struct SignatureSelector<TResult (THost::*)( TArguments... ) volatile> final
 	{
+		// Signature for given member-function type.
 		using Signature = TResult ( TArguments... );
 	};
 
+	/**
+		@brief	Signature selector for pointers to member function with constant volatile context.
+	*/
 	template< typename THost, typename TResult, typename... TArguments >
 	struct SignatureSelector<TResult (THost::*)( TArguments... ) const volatile> final
 	{
+		// Signature for given member-function type.
 		using Signature = TResult ( TArguments... );
 	};
 }

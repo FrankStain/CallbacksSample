1b7bed54a57f7569476bb222662f1d5105e85e9b
Frank Stein <dr.frank.stain@gmail.com>
2022-12-17 23:40:05 +0300

    Docs for internal constants.

--- source/demo/callbacks/internal/constants.h
+++ source/demo/callbacks/internal/constants.h
@@ -7,12 +7,15 @@ inline namespace Callbacks
 {
 namespace Internal
 {
+	// Whether the given candidate is valid global function.
 	template< auto CANDIDATE >
 	inline constexpr bool IS_GLOBAL_FUNCTION = IsGlobalFunction<decltype( CANDIDATE )>::value;
 
+	// Whether the given candidate is valid member function for given host type.
 	template< auto CANDIDATE, typename THost >
 	inline constexpr bool IS_MEMBER_FUNCTION = IsMemberFunction<decltype( CANDIDATE ), THost>::value;
 
+	// Whether the given candidate is valid type of functor.
 	template< typename TCandidate >
 	inline constexpr bool IS_FUNCTOR = IsFunctor<TCandidate>::value;
 }

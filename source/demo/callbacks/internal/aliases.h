5b51a8513c80ec7ecc1ee5bcf137a5c0f14a446e
Frank Stein <dr.frank.stain@gmail.com>
2022-12-17 20:32:20 +0300

    Docs for internal constants.

--- source/demo/callbacks/internal/aliases.h
+++ source/demo/callbacks/internal/aliases.h
@@ -7,11 +7,26 @@ inline namespace Callbacks
 {
 namespace Internal
 {
+	/**
+		@brief	Signature of function by pointer.
+
+		The alias takes non-type template argument, and deduces the type of it signature.
+		For non-function arguments the `void ()` signature will be deducted as shortcut.
+
+		@tparam	CANDIDATE	Arbitrary entity, which signature need to be deducted.
+	*/
 	template< auto CANDIDATE >
 	using FunctionSignature = typename SignatureSelector<decltype( CANDIDATE )>::Signature;
 
-	template< typename TLambda >
-	using FunctorSignature = typename SignatureSelector<decltype( &TLambda::operator() )>::Signature;
+	/**
+		@brief	Signature of functor by type.
+
+		The alias takes type of functor, and deduces the type of it signature.
+
+		@tparam	TFunctor	Type of valid functor, which signature need to be deducted.
+	*/
+	template< typename TFunctor >
+	using FunctorSignature = typename SignatureSelector<decltype( &TFunctor::operator() )>::Signature;
 }
 }
 }

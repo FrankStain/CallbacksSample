9d2e5cc5d24d00505b47f4f9b77abae6dc2baba0
Frank Stein <dr.frank.stain@gmail.com>
2022-12-18 10:19:37 +0300

    Docs for global function context.

--- source/demo/callbacks/internal/GlobalFunctionContext.h
+++ source/demo/callbacks/internal/GlobalFunctionContext.h
@@ -7,11 +7,28 @@ inline namespace Callbacks
 {
 namespace Internal
 {
+	/**
+		@brief	Callback execution context for global functions.
+
+		Context is used by callback instances to produce the proxy function for certain global function.
+		`ProxyCall` inner template provides the valid proxy with common interface for any given global function.
+
+		@tparam	TResult		Result of function.
+		@tparam	TArguments	List of function parameters.
+	*/
 	template< typename TResult, typename... TArguments >
 	struct GlobalFunctionContext final
 	{
+		/**
+			@brief	Adapter-function for given pointer to global function.
+
+			Template adapts the given function to required interface of callback routine.
+			Actual function is remembered sue to instantiation. First argument does not used here due to specific of global functions.
+
+			@tparam	FUNCTION	Pointer to actual global function being called no adapter call.
+		*/
 		template< TResult (*FUNCTION)( TArguments... ) >
-		static inline TResult ProxyCall( void* const, TArguments... arguments )
+		static inline TResult AdaptedCall( void* const, TArguments... arguments )
 		{
 			return FUNCTION( std::forward<TArguments>( arguments )... );
 		};

9557386729044c04fced465396bbb730167f5ee5
Frank Stein <dr.frank.stain@gmail.com>
2022-12-18 15:59:07 +0300

    Docs for member function context.

--- source/demo/callbacks/internal/MemberFunctionContext.h
+++ source/demo/callbacks/internal/MemberFunctionContext.h
@@ -7,38 +7,77 @@ inline namespace Callbacks
 {
 namespace Internal
 {
+	/**
+		@brief	Callback execution context for member functions.
+
+		Context is used by callback instances to produce the proxy function for certain member function.
+		`AdaptedCall` inner templates are used to adapt the given member function to common interface.
+		First argument of `AdaptedCall` accept the instance of type `THost` - the host object for member function to be called.
+
+		@tparam	THost		Type of host class, which objects accept the function call.
+		@tparam	TResult		Result of function.
+		@tparam	TArguments	List of function parameters.
+	*/
 	template< typename THost, typename TResult, typename... TArguments >
 	struct MemberFunctionContext final
 	{
+		/**
+			@brief	Adapter-function for given pointer to member function.
+
+			Template adapts the given function to required interface of callback routine.
+			Actual function is remembered due to instantiation. First argument here represents the host object, which will be used to call the function.
+
+			@tparam	METHOD	Pointer to actual member function being called on adapter call.
+		*/
 		template< TResult (THost::*METHOD)( TArguments... ) >
-		static inline TResult ProxyCall( void* const context, TArguments... arguments )
+		static inline TResult AdaptedCall( void* const context, TArguments... arguments )
 		{
-			THost* host = reinterpret_cast<THost*>( context );
-
+			THost* const host = reinterpret_cast<THost*>( context );
 			return (host->*METHOD)( std::forward<TArguments>( arguments )... );
 		};
 
+		/**
+			@brief	Adapter-function for given pointer to member function.
+
+			Template adapts the given function to required interface of callback routine.
+			Actual function is remembered due to instantiation. First argument here represents the host object, which will be used to call the function.
+
+			@tparam	METHOD	Pointer to actual member function being called on adapter call.
+		*/
 		template< TResult (THost::*METHOD)( TArguments... ) const >
-		static inline TResult ProxyCall( void* const context, TArguments... arguments )
+		static inline TResult AdaptedCall( void* const context, TArguments... arguments )
 		{
-			THost* host = reinterpret_cast<THost*>( context );
-
+			THost* const host = reinterpret_cast<THost*>( context );
 			return (host->*METHOD)( std::forward<TArguments>( arguments )... );
 		};
 
+		/**
+			@brief	Adapter-function for given pointer to member function.
+
+			Template adapts the given function to required interface of callback routine.
+			Actual function is remembered due to instantiation. First argument here represents the host object, which will be used to call the function.
+
+			@tparam	METHOD	Pointer to actual member function being called on adapter call.
+		*/
 		template< TResult (THost::*METHOD)( TArguments... ) volatile >
-		static inline TResult ProxyCall( void* const context, TArguments... arguments )
+		static inline TResult AdaptedCall( void* const context, TArguments... arguments )
 		{
-			THost* host = reinterpret_cast<THost*>( context );
-
+			THost* const host = reinterpret_cast<THost*>( context );
 			return (host->*METHOD)( std::forward<TArguments>( arguments )... );
 		};
 
-		template< TResult (THost::*METHOD)( TArguments... ) const volatile >
-		static inline TResult ProxyCall( void* const context, TArguments... arguments )
-		{
-			THost* host = reinterpret_cast<THost*>( context );
+		/**
+			@brief	Adapter-function for given pointer to member function.
 
+			Template adapts the given function to required interface of callback routine.
+			Actual function is remembered due to instantiation. First argument here represents the host object, which will be used to call the function.
+
+			@tparam	METHOD	Pointer to actual member function being called on adapter call.
+		*/
+		template< TResult (THost::*METHOD)( TArguments... ) const volatile >
+		static inline TResult AdaptedCall( void* const context, TArguments... arguments )
+		{
+			THost* const host = reinterpret_cast<THost*>( context );
 			return (host->*METHOD)( std::forward<TArguments>( arguments )... );
 		};
 	};
